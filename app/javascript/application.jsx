5a196a8d5d17307667c1d618397f351876ca7ed7
lef237 <lef237k5@gmail.com>
2023-02-20 23:09:19 +0900

    コンポーネントを追加・分割した

--- app/javascript/application.jsx
+++ app/javascript/application.jsx
@@ -5,16 +5,19 @@ import "./controllers";
 import React from "react";
 import { createRoot } from "react-dom/client";
 
+import Hey from "./components/Hey"
+
 function App() {
   return (
     <h1>
       <Hello />
+      <Hey />
     </h1>
   );
 }
 
-const Hello = (): string => {
-  return "Hello React TypeScript!";
+const Hello = () => {
+  return <>Hello React TypeScript!!!</>;
 };
 
 const root = document.getElementById("root");
