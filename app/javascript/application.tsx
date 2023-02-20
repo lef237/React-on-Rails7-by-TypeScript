6de13379288a30ac7596f361ef1121f26258882b
lef237 <lef237k5@gmail.com>
2023-02-20 19:58:20 +0900

    Rails7でReact(tsx)を動かすことに成功

---
+++ app/javascript/application.tsx
@@ -0,0 +1,22 @@
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers"
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+function App() {
+  return (
+    <h1>Hello React TypeScript!</h1>
+  )
+}
+
+const root = document.getElementById('root');
+if (!root) {
+  throw new Error('No root element');
+}
+createRoot(root).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
