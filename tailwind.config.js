7ebb432d5546e1a43012abe927b62cab0a76fd3e
lef237 <lef237k5@gmail.com>
2023-02-20 20:14:27 +0900

    tailwind.config.js内にslimとtsxを追加してcssが適用されるようにした

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   content: [
     './app/views/**/*.html.erb',
+    './app/views/**/*.html.slim',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
+    './app/javascript/**/*.js',
+    './app/javascript/**/*.tsx'
   ]
 }
